2946e4f4f12d00c664642073d69277e4fdd9d2a0
Brittany Kerr and Rena Rallis <brittany@>
2017-06-01 09:55:21 -0700

    place html, js, css in container to respond to height prompt

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  var height = parseInt(prompt("How tall are you?"));
+
+  if (height > 5) {
+    $('#rides').show();
+  } else if (height === 5) {
+    alert("Now don't go crazy!");
+    $('#rides').show();
+  } else {
+    $('#under-5').show();
+  }
+});
