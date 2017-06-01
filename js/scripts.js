506d40915c257efc6d789ae2e9fc7833e4709c09
Brittany Kerr and Rena Rallis <rena@>
2017-06-01 10:55:41 -0700

    add distinct background colors for branches

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
   var height = parseFloat(prompt("How tall are you?"));
 
   if (height > 5) {
-    $('#rides').show();
+    $('#rides').addClass("blue-background");
   } else if (height === 5) {
     alert("Now don't go crazy!");
-    $('#rides').show();
+    $('#rides').addClass("blue-background");
   } else {
-    $('#under-5').show();
+    $('#under-5').addClass("red-background");
   }
 });
